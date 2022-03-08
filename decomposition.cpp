42ebea85be859f919164f7e1628e4f707dc60b66
Onecode24 <angelo24yanve@gmail.com>
2022-03-08 09:24:32 +0100

    calcul du phi

--- decomposition.cpp
+++ decomposition.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 void addNumber(map<int, int>* a,int b);
+int phi(int p,int e);
 
 int main(){
     map<int, int> keys;
     int n;cin>>n;
+    int y(n);
      for(int i(2); i<=n; i++){
          bool a=true;
          while(a && n>1){
@@ -19,13 +22,15 @@ int main(){
              }else a=false;
          }
      }
-
+    int result(1);
     auto itr=keys.begin();
     while(itr!=keys.end()){
         cout<< itr->first << " "<< itr->second<<endl;
+        result*=phi(itr->first,itr->second);
         itr++;
     }
 
+    cout << "phi de "<< y << " est "<< result<<endl;
     return 0;
 }
 
@@ -40,4 +45,9 @@ void addNumber(map<int, int>* a,int b){
         itr++;
     }
     if(c) a->insert({b,1});
+}
+
+int phi(int p,int e){
+    if(e==1) return p-1;
+    else return (p-1)*pow(p,e-1);
 }
